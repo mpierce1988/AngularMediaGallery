0f8a5e6fc9ba2550fff4aec95f76994bd6e46b33
Michael Pierce <michael.todd.pierce@gmail.com>
2023-07-29 21:47:17 -0300

    Basic logic for register component, need to flesh out

--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  form: FormGroup = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.email])
+  });
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    // Validate form is valid
+    if(this.form.valid){
+      this.authService.register(this.form.value.email, this.form.value.password)
+        .subscribe({
+          next: (response) => {
+            console.log(response);
+            // Handle successful registration
+          },
+          error: (error) => {
+            console.log(error);
+            // Handle error
+          }
+        });
+    }
+  }
 }
