a0707fb03c870a5acc3b6ff716b30c14c0b51c1e
Michael Pierce <michael.todd.pierce@gmail.com>
2023-07-29 21:51:21 -0300

    Created basic login component, need to flesh out with error messaging

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  form: FormGroup = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    // Validate form is valid
+    if(this.form.valid){
+      this.authService.login(this.form.value.email, this.form.value.password)
+        .subscribe({
+          next: (response) => {
+            console.log(response);
+            // Handle successful login
+          },
+          error: (error) => {
+            console.log(error);
+            // Handle Error
+          }
+        });
+    }
+  }
 }
