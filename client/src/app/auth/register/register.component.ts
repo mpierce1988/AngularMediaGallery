b3cb5ca4d397fdbb054cb66e24ea48c3c4431200
Michael Pierce <michael.todd.pierce@gmail.com>
2023-07-30 16:37:13 -0300

    Created register component

--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -8,6 +8,10 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  // Create public property to store errors, success message, and loading state
+  errors: string[] = [];
+  successMessage: string = '';
+  loading: boolean = false;
 
   form: FormGroup = new FormGroup({
     email: new FormControl('', Validators.required),
@@ -19,15 +23,25 @@ export class RegisterComponent {
   onSubmit() {
     // Validate form is valid
     if(this.form.valid){
+      // Set loading state
+      this.loading = true;
       this.authService.register(this.form.value.email, this.form.value.password)
         .subscribe({
           next: (response) => {
             console.log(response);
-            // Handle successful registration
+            // Set Success message
+            this.successMessage = 'Registration successful!';
+            // Clear error message
+            this.errors = [];
+            // Set loading state
+            this.loading = false;
           },
           error: (error) => {
             console.log(error);
-            // Handle error
+            // Set error messages
+            this.errors = error.error.errors;
+            // Set loading state
+            this.loading = false;
           }
         });
     }
